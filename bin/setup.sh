fe6103b2cc98ce904b9f25d9a29e0d8c2eaee59b
zhonghui <zhonghuipro@qq.com>
2022-07-18 10:05:06 +0800

    增加小标题

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,6 @@
 sudo gem install bundler jekyll
-bundle init
-bundle add jekyll
-bundle add jekyll-feed
-bundle add jekyll-theme-minima
-bundle add  kramdown-parser-gfm
+#bundle init
+#bundle add jekyll
+#bundle add jekyll-feed
+#bundle add jekyll-theme-minima
+#bundle add  kramdown-parser-gfm
